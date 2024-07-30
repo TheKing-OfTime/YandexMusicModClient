6b793198fba9667bf1eb55b2ba031b54f573cedf
TheKing-OfTime <artem.matvienko0@gmail.com>
2024-07-30 20:38:13 +0300

    Migrate to 5.8.2: chunks sync phase

---
+++ src/app/_next/static/chunks/435.b883b77ccbcb05cb.js
@@ -0,0 +1,72 @@
+"use strict";
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [435],
+  {
+    70435: function (e, t, o) {
+      o.d(t, {
+        config: function () {
+          return c;
+        },
+      });
+      var n = o(49080),
+        s = o(21535);
+      let a = () => ({
+          rumId: "ru.music.frontend",
+          project: "music.frontend.desktop",
+          service: "frontend-desktop",
+          platform: "desktop",
+        }),
+        r = () => ({ project: "music.frontend.desktop", platform: "desktop" }),
+        c = () => {
+          var e, t;
+          return {
+            client: "YandexMusicWeb/1.0",
+            dev: { panel: !0 },
+            logger: {
+              uaDisabled: (e = !!s.env.SERVER_UNIFIED_AGENT_DISABLED),
+              uaServerAddress:
+                null !== (t = s.env.SERVER_UNIFIED_AGENT_ADDRESS) &&
+                void 0 !== t
+                  ? t
+                  : "http://localhost:12344",
+              errorBooster: {
+                ...r(),
+                page: "window.location.pathname",
+                version: s.env.APP_VERSION || "",
+                environment: "production",
+                unhandledRejection: !0,
+                uncaughtException: !0,
+                resourceFails: !0,
+              },
+            },
+            oauthCredentials: {
+              host: "https://oauth.yandex.".concat(n.M),
+              clientId: "97fe03033fa34407ac9bcf91d5afed5b",
+            },
+            tvm: {
+              tvmtoolAuthToken: s.env.TVMTOOL_LOCAL_AUTHTOKEN,
+              self: "music_test",
+              destinations: ["blackbox"],
+              host: "http://localhost:8001",
+              enabled: !1,
+            },
+            rumSettings: {
+              ...a(),
+              page: "window.location.pathname",
+              heroElement: "body",
+              version: s.env.APP_VERSION || "",
+              environment: "production",
+            },
+            passportCredentials: {
+              host: "https://passport.yandex.".concat(n.M),
+              origin: "music_desktop",
+            },
+            blackbox: {
+              host: "http://blackbox.envoy.localhost:9080",
+              enabled: !1,
+            },
+          };
+        };
+    },
+  },
+]);
