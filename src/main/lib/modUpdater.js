3be7cd670dc88db107c28606d6aa7d64511c8a9c
TheKing-OfTime <artem.matvienko0@gmail.com>
2024-08-08 18:03:22 +0300

    Try fix autoupdates again...

--- src/main/lib/modUpdater.js
+++ src/main/lib/modUpdater.js
@@ -1,84 +1,96 @@
 const fetch = require("node-fetch");
-const fsPromise = require('fs').promises;
+const fsPromise = require("fs").promises;
 const path = require("path");
 const semver = require("semver");
 const Logger_js_1 = require("../packages/logger/Logger.js");
 const config_js_1 = require("../config.js");
 
 const UPDATE_CHECK_URL = `https://api.github.com/repos/TheKing-OfTime/YandexMusicModClient/releases/tags/${config_js_1.config.modification.branch}`;
-const APP_ASAR_PATH = path.join(process.env.LOCALAPPDATA,'\\Programs\\YandexMusic\\resources\\app.asar');
-const APP_ASAR_UPDATE_PATH = path.join(process.env.LOCALAPPDATA,'\\Programs\\YandexMusic\\resources\\update.asar');
+const APP_ASAR_PATH = path.join(
+  process.env.LOCALAPPDATA,
+  "\\Programs\\YandexMusic\\resources\\app.asar",
+);
+const APP_ASAR_UPDATE_PATH = path.join(
+  process.env.LOCALAPPDATA,
+  "\\Programs\\YandexMusic\\resources\\update.asar",
+);
 const currentVersion = config_js_1.config.modification.version;
-console.log(APP_ASAR_PATH)
-
+console.log(APP_ASAR_PATH);
 
 class ModUpdater {
-    latestVersion = null;
-    updaterId = null;
-    onModUpdateListeners = [];
-    logger;
-    constructor() {
-        this.logger = new Logger_js_1.Logger("ModUpdaterLogger");
-        this.logger.log("Initializing");
-    }
+  latestVersion = null;
+  updaterId = null;
+  onModUpdateListeners = [];
+  logger;
+  constructor() {
+    this.logger = new Logger_js_1.Logger("ModUpdaterLogger");
+    this.logger.log("Initializing");
+  }
 
-    start() {
-        this.checkAndInstall();
-        this.updaterId = setInterval(() => {
-            this.checkAndInstall();
-        }, config_js_1.config.common.UPDATE_POLL_INTERVAL_MS);
-        this.logger.log("Loop started");
+  start() {
+    this.checkAndInstall();
+    this.updaterId = setInterval(() => {
+      this.checkAndInstall();
+    }, config_js_1.config.common.UPDATE_POLL_INTERVAL_MS);
+    this.logger.log("Loop started");
+  }
+  stop() {
+    if (this.updaterId) {
+      clearInterval(this.updaterId);
+      this.logger.log("Loop stopped");
     }
-    stop() {
-        if (this.updaterId) {
-            clearInterval(this.updaterId);
-            this.logger.log("Loop stopped");
-        }
-    }
+  }
 
-    async checkAndInstall() {
-        const url = await this.checkForUpdates()
-        if (!url) return;
+  async checkAndInstall() {
+    const url = await this.checkForUpdates();
+    if (!url) return;
 
-        await this.downloadFile(url, APP_ASAR_PATH);
-        //await this.deleteFile(APP_ASAR_PATH)
-        //await this.renameFile(APP_ASAR_UPDATE_PATH, APP_ASAR_PATH);
-        this.onModUpdateListeners.forEach((listener) => {
-            listener(currentVersion, this.latestVersion);
-        });
-    }
+    await this.downloadFile(url, APP_ASAR_PATH);
+    //await this.deleteFile(APP_ASAR_PATH)
+    //await this.renameFile(APP_ASAR_UPDATE_PATH, APP_ASAR_PATH);
+    setTimeout(() => {                                      //That's disgusting fix. I know...
+      this.onModUpdateListeners.forEach((listener) => {
+        listener(currentVersion, this.latestVersion);
+      });
+    }, 20000);
+  }
 
-    async checkForUpdates() {
-        const response = await fetch(UPDATE_CHECK_URL);
-        const releaseData = await response.json()
-        this.latestVersion = releaseData.name;
-        if (semver.lt(currentVersion, this.latestVersion)) {
-            this.logger.log("New version available:", currentVersion, "->", this.latestVersion);
-            return releaseData.assets[0].browser_download_url;
-        }
-        return false;
+  async checkForUpdates() {
+    const response = await fetch(UPDATE_CHECK_URL);
+    const releaseData = await response.json();
+    this.latestVersion = releaseData.name;
+    if (semver.lt(currentVersion, this.latestVersion)) {
+      this.logger.log(
+        "New version available:",
+        currentVersion,
+        "->",
+        this.latestVersion,
+      );
+      return releaseData.assets[0].browser_download_url;
     }
+    return false;
+  }
 
-    async downloadFile(url, path) {
-        const res = await fetch(url);
-        const buffer = await res.buffer();
-        await fsPromise.writeFile(path, buffer);
-        this.logger.log("Downloaded. Restart to apply changes");
-    }
+  async downloadFile(url, path) {
+    const res = await fetch(url);
+    const buffer = await res.buffer();
+    await fsPromise.writeFile(path, buffer);
+    this.logger.log("Downloaded. Restart to apply changes");
+  }
 
-    async deleteFile(path) {
-        await fsPromise.unlink(path);
-        this.logger.log("Deleted: ", path);
-    }
+  async deleteFile(path) {
+    await fsPromise.unlink(path);
+    this.logger.log("Deleted: ", path);
+  }
 
-    async renameFile(oldPath, newPath) {
-        await fsPromise.rename(oldPath, newPath);
-        this.logger.log("Renamed: ", oldPath, " to ", newPath)
-    }
+  async renameFile(oldPath, newPath) {
+    await fsPromise.rename(oldPath, newPath);
+    this.logger.log("Renamed: ", oldPath, " to ", newPath);
+  }
 
-    onUpdate(listener) {
-        this.onModUpdateListeners.push(listener);
-    }
+  onUpdate(listener) {
+    this.onModUpdateListeners.push(listener);
+  }
 }
 
 module.exports = ModUpdater;
