9174880873871e3b52596bed07849a23f04dc1b1
TheKing-OfTime <artem.matvienko0@gmail.com>
2024-11-15 23:11:49 +0300

    Migration to 5.28.4: Bump version

--- src/main/config.js
+++ src/main/config.js
@@ -27,7 +27,7 @@ exports.config = {
     clickErrorCounter: "690.2354",
   },
   modification: {
-      version: "1.16.5",
+      version: "1.17.0",
   },
 };
 const applyCommonConfig = (commonConfig) => {
