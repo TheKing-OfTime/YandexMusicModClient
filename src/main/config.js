58857f1342dc6f868a4e593dd3ae397d9e7731dc
TheKing-OfTime <artem.matvienko0@gmail.com>
2025-01-16 00:22:18 +0300

    Bump version

--- src/main/config.js
+++ src/main/config.js
@@ -27,7 +27,7 @@ exports.config = {
     clickErrorCounter: "690.2354",
   },
   modification: {
-      version: "1.18.4",
+      version: "1.18.5",
   },
 };
 const applyCommonConfig = (commonConfig) => {
