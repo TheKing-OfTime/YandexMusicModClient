ab7d7bc6d403efe01e70dac5e28d8638c9233a83
TheKing-OfTime <artem.matvienko0@gmail.com>
2025-02-14 21:39:24 +0300

    Fix syntax err

--- src/main/config.js
+++ src/main/config.js
@@ -27,7 +27,7 @@ exports.config = {
     clickErrorCounter: "690.2354",
   },
   modification: {
-      version: "1.20.8",
+      version: "1.20.9",
   },
 };
 const applyCommonConfig = (commonConfig) => {
