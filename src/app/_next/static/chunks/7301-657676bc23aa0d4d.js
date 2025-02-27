b71d9a91570ea92598856e114ac7a0b8b9396cdb
TheKing-OfTime <artem.matvienko0@gmail.com>
2025-02-28 00:20:16 +0300

    Fix

--- src/app/_next/static/chunks/7301-657676bc23aa0d4d.js
+++ src/app/_next/static/chunks/7301-657676bc23aa0d4d.js
@@ -9886,7 +9886,7 @@
                     (0, _.jsx)(N.wx, {
                       title: `${Math.round((r?.volume ?? 0) * 100)}%`,
                       children: (0, _.jsxs)("div", {
-                        children: (0, _.jsx)(iv.F, { primaryValue: window?.ENABLE_YNISON_REMOTE_CONTROL ? window.r?.volume : undefined }),
+                        children: (0, _.jsx)(iv.F, { primaryValue: window?.ENABLE_YNISON_REMOTE_CONTROL ? r?.volume : undefined }),
                       }),
                     }),
                   ],
