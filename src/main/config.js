87ad90669cc35ba368a8128c1692bb1c46232ff0
TheKing-OfTime <artem.matvienko0@gmail.com>
2025-03-06 23:13:10 +0300

    Bump version & Change PATCHNOTES.md

--- src/main/config.js
+++ src/main/config.js
@@ -27,7 +27,7 @@ exports.config = {
     clickErrorCounter: "690.2354",
   },
   modification: {
-      version: "1.23.0",
+      version: "1.23.1",
   },
 };
 const applyCommonConfig = (commonConfig) => {
