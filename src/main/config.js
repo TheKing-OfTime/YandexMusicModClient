bb4af08db649ea7539f0167e3f3b0912fc266164
TheKing-OfTime <artem.matvienko0@gmail.com>
2025-03-13 08:47:49 +0300

    Update PATCHNOTES.md and mod version to 1.23.4

--- src/main/config.js
+++ src/main/config.js
@@ -27,7 +27,7 @@ exports.config = {
     clickErrorCounter: "690.2354",
   },
   modification: {
-      version: "1.23.3",
+      version: "1.23.4",
   },
 };
 const applyCommonConfig = (commonConfig) => {
