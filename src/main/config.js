2f53063f209e7cac9b7acd0ffd8da493b3997f6c
TheKing-OfTime <artem.matvienko0@gmail.com>
2025-03-21 18:06:25 +0300

    Spoof version

--- src/main/config.js
+++ src/main/config.js
@@ -27,7 +27,7 @@ exports.config = {
     clickErrorCounter: "690.2354",
   },
   modification: {
-      version: "1.23.5",
+      version: "1.23.6",
   },
 };
 const applyCommonConfig = (commonConfig) => {
