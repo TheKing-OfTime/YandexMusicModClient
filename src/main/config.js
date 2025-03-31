473be55908771b5c4fd8b61c92e319c5e8462338
TheKingOfTime <artem.matvienko0@gmail.com>
2025-03-31 21:00:02 +0300

    chore: Update version to 5.44.3 and 1.23.9

--- src/main/config.js
+++ src/main/config.js
@@ -27,7 +27,7 @@ exports.config = {
     clickErrorCounter: "690.2354",
   },
   modification: {
-      version: "1.23.8",
+      version: "1.23.9",
   },
 };
 const applyCommonConfig = (commonConfig) => {
