f4898d692cfc858989f84bfe6d6e0fdaffb90484
TheKingOfTime <artem.matvienko0@gmail.com>
2025-04-05 15:46:14 +0300

    chore: Update version to 1.24.1 and modify PATCHNOTES.md

--- src/main/config.js
+++ src/main/config.js
@@ -27,7 +27,7 @@ exports.config = {
     clickErrorCounter: "690.2354",
   },
   modification: {
-      version: "1.24.0",
+      version: "1.24.1",
   },
 };
 const applyCommonConfig = (commonConfig) => {
