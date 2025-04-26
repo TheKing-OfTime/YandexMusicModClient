ee1c8155559ddee26b7c2782cfa2621ed2c9ceb8
TheKingOfTime <artem.matvienko0@gmail.com>
2025-04-26 21:41:30 +0300

    fix: remove extra expression

--- src/main/lib/scrobble/index.js
+++ src/main/lib/scrobble/index.js
@@ -6,7 +6,6 @@ const ScrobblerManager_1 = require("./ScrobblerManager");
 const LastFmScrobbler_1 = require("./services/lastfm/LastFmScrobbler");
 const scrobblerManager = new ScrobblerManager_1.ScrobblerManager();
 const config_1 = require("../../config");
-config_1.config
 exports.scrobblerManager = scrobblerManager;
 scrobblerManager.registerScrobbler(
 new LastFmScrobbler_1.LastFmScrobbler(config_1.config.modification.lastfm.apikey, config_1.config.modification.lastfm.sharedSecret, config_1.config.modification.lastfm.baseUrl));
