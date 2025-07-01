aadb7a458b53bfb3186a51fb855f88a0c8f5558f
TheKingOfTime <artem.matvienko0@gmail.com>
2025-07-01 16:27:37 +0300

    chore: Prepare to new release

--- src/main/config.js
+++ src/main/config.js
@@ -27,7 +27,7 @@ exports.config = {
     clickErrorCounter: "690.2354",
   },
   modification: {
-    version: "1.28.0",
+    version: "1.28.1",
     realYMVersion: "5.57.0",
     lastfm: {
       apikey: "2b9046ddec8c00edcd0144b3abbea4ba",
