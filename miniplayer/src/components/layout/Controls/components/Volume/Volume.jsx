8bd6282942aa771f39541a5aad9ef095c93b46a9
TheKingOfTime <artem.matvienko0@gmail.com>
2026-01-01 14:16:45 +0300

    feat: Add onWheel to Volume slider

--- miniplayer/src/components/layout/Controls/components/Volume/Volume.jsx
+++ miniplayer/src/components/layout/Controls/components/Volume/Volume.jsx
@@ -15,13 +15,20 @@ export default function Volume({ value=0.5, variant = 'default', style=undefined
         window.desktopEvents?.send('MINIPLAYER_PLAYER_ACTION', 'SET_VOLUME', value);
     }, []);
 
+    const onWheel = useCallback((e) => {
+        e.preventDefault();
+        let newVolume = (value ?? 0) + (e.deltaY < 0 ? 0.01 : -0.01);
+        newVolume = Math.min(1, Math.max(0, newVolume));
+        onChange(newVolume);
+    }, [value, onChange]);
+
     const toggleMute = useCallback(() => {
         setUnmuteVolume(value);
         onChange(value ? 0 : unmuteVolume);
     }, [value, unmuteVolume, setUnmuteVolume, onChange]);
 
     return (
-        <div className={'Volume_container ' + (variant ? 'Volume_container_' + variant : '')} style={style}>
+        <div className={'Volume_container ' + (variant ? 'Volume_container_' + variant : '')} style={style} onWheel={onWheel}>
             <button className={'Volume_MuteButton ' + (!alwaysShow ? 'Volume_hover' : '')} onClick={toggleMute}>
                 <Icon size={28} name={(value ?? 0) > 0 ? 'volume_xs' : 'volumeOff_xs'} />
             </button>
